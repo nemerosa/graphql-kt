e92f6374711dcc7bc8b0115ce4fc8c351365c148
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-09-09 11:59:13 +0200

    GraphQL model - int field

--- src/main/kotlin/net/nemerosa/ontrack/graphql/_GraphQL.kt
+++ src/main/kotlin/net/nemerosa/ontrack/graphql/_GraphQL.kt
@@ -5,7 +5,31 @@ import kotlin.reflect.KClass
 /**
  * Field
  */
-interface Field<C : Any, F : Any>
+interface Field<C : Any, F : Any> {
+    val name: String
+    val description: String?
+}
+
+abstract class AbstractField<C : Any, F : Any>(
+        override val name: String,
+        override val description: String? = null)
+    : Field<C, F>
+
+abstract class ScalarField<C : Any, F : Any>(
+        name: String,
+        description: String? = null,
+        private val getter: C.() -> F
+) : AbstractField<C, F>(name, description)
+
+/**
+ * Integer field
+ */
+
+class IntField<C : Any>(
+        name: String,
+        description: String? = null,
+        getter: C.() -> Int
+) : ScalarField<C, Int>(name, description, getter)
 
 /**
  * Type
@@ -35,6 +59,12 @@ interface RootQueryDef<F : Any> {
 class TypeBuilder<C : Any>(
         private val cls: KClass<C>
 ) {
+    private val fields = mutableListOf<Field<C, *>>()
+
+    fun field(field: Field<C, *>) {
+        fields += field
+    }
+
     val type: Type<C>
         get() = Type(cls)
 
@@ -44,4 +74,14 @@ inline fun <reified C : Any> objectType(init: TypeBuilder<C>.() -> Unit): Type<C
     val builder = TypeBuilder(C::class)
     builder.init()
     return builder.type
+}
+
+fun <C : Any> TypeBuilder<C>.fieldInt(name: String, description: String?, getter: C.() -> Int) {
+    field(
+            IntField<C>(
+                    name,
+                    description,
+                    getter
+            )
+    )
 }
--- src/main/kotlin/net/nemerosa/ontrack/schema/_OntrackSchema.kt
+++ src/main/kotlin/net/nemerosa/ontrack/schema/_OntrackSchema.kt
@@ -1,10 +1,21 @@
 package net.nemerosa.ontrack.schema
 
 import net.nemerosa.ontrack.graphql.*
+import net.nemerosa.ontrack.model.Branch
 import net.nemerosa.ontrack.model.Project
 import net.nemerosa.ontrack.model.StructureService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+
+/**
+ * Branch type
+ */
+@Component
+class TypeBranch
+    : TypeDef<Branch> {
+    override val type: Type<Branch>
+        get() = objectType { }
+}
 
 /**
  * Project type
@@ -17,7 +28,7 @@ constructor(
 ) : TypeDef<Project> {
     override val type: Type<Project>
         get() = objectType {
-            // TODO fieldInt(Project::id)
+            fieldInt("id", "Project ID", Project::id)
             // TODO fieldString(Project::name)
             // TODO fieldString(Project::description)
             // TODO fieldBoolean(Project::disabled)
